c5a58e86efd060fd0063dc35421ede1c05a07aea
nantongchenfei <31747978+nantongchenfei@users.noreply.github.com>
2018-05-04 14:32:08 -0400

    AWA training and test data
    data should be unzipped from the zip file first. 
    the script runs several times with different parameters to search for the best parameter ' r' and 'b'

---
+++ AWA2/testLSH.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 5 1 > AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 10 1 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 15 1 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 20 1 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 25 1 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 5 2 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 10 2 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 15 2 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 20 2 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 25 2 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 5 4 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 10 4 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 15 4 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 20 4 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 25 4 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 5 8 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 10 8 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 15 8 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 20 8 >> AWA2record.txt
+time python3 LSH_test_my.py AWA_Xtr.csv AWA_Ytr.csv AWA_Xte.csv AWA_Yte.csv 25 8 >> AWA2record.txt
