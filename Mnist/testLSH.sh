174dd60e87a01fbb6301c64980d35fd4723c34de
nantongchenfei <31747978+nantongchenfei@users.noreply.github.com>
2018-05-04 14:37:55 -0400

    Add files via upload

---
+++ Mnist/testLSH.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 5 1 > Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 10 1 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 15 1 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 20 1 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 25 1 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 5 2 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 10 2 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 15 2 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 20 2 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 25 2 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 5 4 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 10 4 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 15 4 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 20 4 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 25 4 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 5 8 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 10 8 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 15 8 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 20 8 >> Mnistrecord.txt
+time python3 LSH_test_my.py Mnist_Xtrain.csv Mnist_Ytrain.csv Mnist_Xtest.csv Mnist_Ytest.csv 25 8 >> Mnistrecord.txt
